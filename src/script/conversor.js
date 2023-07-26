fc557c90dbb5579c6460d8ad269ff2e4d926bb3c
Felipe <geloseco331@gmail.com>
2023-07-26 14:59:51 -0300

    atualização

--- src/script/conversor.js
+++ src/script/conversor.js
@@ -1,28 +1,23 @@
 function converterMoeda() {
     let quantiaEmReal = parseFloat(document.querySelector('#quantiaEmReal').value);
     let moeda = document.querySelector('#moeda').value;
-    let taxaDeConversao;
 
     switch (moeda) {
         case 'dolar':
-            taxaDeConversao = 4.73;
+            quantiaEmReal /= 4.73;
             break;
         case 'euro':
-            taxaDeConversao = 5.24;
+            quantiaEmReal /= 5.24;
             break;
         case 'iene':
-            taxaDeConversao = 0.034;
+            quantiaEmReal /= 0.034;
             break;
         case 'peso':
-            taxaDeConversao = 0.017;
-            break;
-        default:
-            taxaDeConversao = 1;
+            quantiaEmReal /= 0.017;
             break;
     }
 
-    let quantiaConvertida = quantiaEmReal / taxaDeConversao;
-    document.querySelector('#quantiaConvertida').value = quantiaConvertida.toFixed(2);
+    document.querySelector('#quantiaConvertida').value = quantiaEmReal.toFixed(2);
 }
 
 document.querySelector('#quantiaEmReal').addEventListener('input', converterMoeda);
