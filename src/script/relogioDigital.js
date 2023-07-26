f8a554eae1ed97f901048fdaeafb6cae03935aee
Felipe <geloseco331@gmail.com>
2023-07-26 11:15:59 -0300

    atualizações

--- src/script/relogioDigital.js
+++ src/script/relogioDigital.js
@@ -3,7 +3,7 @@ function updateClock() {
     let hours = String(now.getHours()).padStart(2, '0');
     let minutes = String(now.getMinutes()).padStart(2, '0');
     let seconds = String(now.getSeconds()).padStart(2, '0');
-    document.getElementById('clock').innerHTML = `${hours}:${minutes}:${seconds}`;
+    document.querySelector('.clock').innerHTML = `${hours}:${minutes}:${seconds}`;
   }
   
   setInterval(updateClock, 1000);
