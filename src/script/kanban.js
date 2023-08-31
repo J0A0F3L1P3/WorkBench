d19354fed38ffad0ededb8bbfa67c7cbf9e4cd63
Felipe <geloseco331@gmail.com>
2023-08-31 15:22:49 -0300

    adição do kanba

---
+++ src/script/kanban.js
@@ -0,0 +1,30 @@
+let tasks = document.querySelectorAll('.task');
+
+tasks.forEach(task => {
+    task.addEventListener('dragstart', dragStart);
+    task.addEventListener('dragend', dragEnd);
+});
+
+function dragStart() {
+    this.classList.add('dragging');
+}
+
+function dragEnd() {
+    this.classList.remove('dragging');
+}
+
+const columns = document.querySelectorAll('.card-body');
+
+columns.forEach(column => {
+    column.addEventListener('dragover', dragOver);
+    column.addEventListener('drop', drop);
+});
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function drop() {
+    let task = document.querySelector('.dragging');
+    this.appendChild(task);
+}
